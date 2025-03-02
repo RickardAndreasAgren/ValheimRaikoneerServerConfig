ea7f75768a0c37f403540380d2744fdaeb357c1b
Rickard Ågren <r.aagren@hotmail.com>
2025-03-02 22:18:48 +0100

    TreeSpiders are not meant to play we think.

---
+++ Install.ps1
@@ -0,0 +1,54 @@
+﻿
+
+$here = pwd
+$tp = Test-Path "$here\..\BepInEx"
+if(-not $tp) {
+ Write-Host "Why am I not in the BepInEx directory?"
+ 
+ Write-Host -NoNewLine 'Press any key to continue...';
+ $null = $Host.UI.RawUI.ReadKey('NoEcho,IncludeKeyDown');
+ return 0
+}
+
+$tp = Test-Path "$here\plugins\MonsterLabZConfig"
+if($tp -eq $true) {
+    Write-Host "If you want to do a fresh install, remove MonsterLabZConfig and RustyMods-BestiaryMod and redownload plugins in Thunderstore first."
+	
+	Write-Host -NoNewLine 'Press any key to continue...';
+	$null = $Host.UI.RawUI.ReadKey('NoEcho,IncludeKeyDown');
+	return 0
+}
+
+
+$tp = Test-Path "$here\plugins\RustyMods-Bestiary"
+if(-not $tp) {
+ Write-Host "Bestiary must be installed for the first part of installation, import the mod configuration code in Thunderstore"
+ Write-Host -NoNewLine 'Press any key to continue...';
+ $null = $Host.UI.RawUI.ReadKey('NoEcho,IncludeKeyDown');
+ return 0
+}
+$bestiaryPath = "$here\plugins\RustyMods-Bestiary"
+
+$tp = Test-Path "$here\plugins\MonsterLabZ"
+$tp2 = Test-Path "$here\plugins\MonsterLabZFix-MonsterLabZ"
+if(-not $tp -and -not $tp2) {
+ Write-Host "MonsterLabZ must be installed for the first part of installation, import the mod configuration code in Thunderstore"
+ Write-Host -NoNewLine 'Press any key to continue...';
+ $null = $Host.UI.RawUI.ReadKey('NoEcho,IncludeKeyDown');
+ return 0
+}
+if($tp -eq $true) {
+    $mlzPath = "$here\plugins\MonsterLabZ"
+}
+if($tp2 -eq $true) {
+    $mlzPath = "$here\plugins\MonsterLabZFix-MonsterLabZ"
+}
+
+Move-Item "$mlzPath\MonsterLabZ.dll" "$here\installplugins\MonsterLabZConfig\Managed_Data\MonsterLabZ.libassembly"
+Move-Item "$bestiaryPath\Bestiary.dll" "$here\installplugins\RustyMods-BestiaryModR\Managed_Data\bestiary.libassembly"
+
+Write-Host "Proceed to uninstall MonsterLabZ and RustyMods-BestiaryMod from Thunderstore and then run InstallSecond.ps1"
+
+Write-Host -NoNewLine 'Press any key to finish...';
+$null = $Host.UI.RawUI.ReadKey('NoEcho,IncludeKeyDown');
+return 0
---
+++ InstallSecond.ps1
@@ -0,0 +1,35 @@
+﻿
+$here = pwd
+
+$tp = Test-Path "$here\plugins\RustyMods-Bestiary"
+if($tp) {
+  Write-Host "Uninstall Bestiary in Thunderstore"
+  Write-Host -NoNewLine 'Press any key to continue...';
+  $null = $Host.UI.RawUI.ReadKey('NoEcho,IncludeKeyDown');
+  return 0
+}
+$bestiaryPath = "$here\plugins\RustyMods-Bestiary"
+
+$tp = Test-Path "$here\plugins\MonsterLabZ"
+$tp2 = Test-Path "$here\plugins\MonsterLabZFix-MonsterLabZ"
+if($tp -or $tp2) {
+  Write-Host "Uninstall MonsterLabZ in Thunderstore"
+  Write-Host -NoNewLine 'Press any key to continue...';
+  $null = $Host.UI.RawUI.ReadKey('NoEcho,IncludeKeyDown');
+  return 0
+}
+if($tp -eq $true) {
+    $mlzPath = "$here\plugins\MonsterLabZ"
+}
+if($tp2 -eq $true) {
+    $mlzPath = "$here\plugins\MonsterLabZFix-MonsterLabZ"
+}
+
+$iplugins = "$here\Installplugins\"
+Move-Item "$iplugins*" .\plugins
+
+Write-Host "You are good to go. Do not play with Slem."
+
+Write-Host -NoNewLine 'Press any key to continue...';
+$null = $Host.UI.RawUI.ReadKey('NoEcho,IncludeKeyDown');
+return 0
